1847916a52c460cf21f7bc18ca7c3fd6e9da8e90
Saugat Shahi <dev.shahi.apps@gmail.com>
2025-12-03 22:18:09 +0545

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import './styles/index.css'
+
+
+function App() {
+  return (
+    <>
+      <p className="bg-red-50">Aura Interiors Frontend</p>
+    </>
+  );
+}
+
+export default App;
